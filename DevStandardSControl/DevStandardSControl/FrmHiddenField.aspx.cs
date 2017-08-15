90c3e1c557aa1bbbc8a2bb7882622c855062f7f0
이건준(gunjun Lee) <ac989832@gmail.com>
2017-08-15 22:16:09 +0900

    .NET Study Book Chapter 6 Complete

---
+++ DevStandardSControl/DevStandardSControl/FrmHiddenField.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevStandardSControl
+{
+    public partial class FrmHiddenField : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                this.ctlHidden.Value = DateTime.Now.ToShortTimeString();
+            }
+        }
+
+        protected void btnOK_Click(object sender, EventArgs e)
+        {
+            Response.Write(ctlHidden.Value);
+        }
+    }
+}
