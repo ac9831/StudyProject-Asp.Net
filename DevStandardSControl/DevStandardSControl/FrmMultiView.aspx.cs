a4ce48e5b623a9a3c74adaf1f2d0413f8b3d173d
이건준(gunjun Lee) <ac989832@gmail.com>
2017-08-20 15:12:04 +0900

    Chapter 8 Study Complete.

---
+++ DevStandardSControl/DevStandardSControl/FrmMultiView.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevStandardSControl
+{
+    public partial class FrmMultiView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                ctlMultiView1.ActiveViewIndex = 0;
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            this.ctlMultiView1.ActiveViewIndex = 1;
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.ctlMultiView1.ActiveViewIndex = 0;
+        }
+    }
+}
---
+++ DevStandardSControl/DevStandardSControl/FrmSubstitution.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevStandardSControl
+{
+    public partial class ㄹ그녀ㅠㄴ샤셔샤ㅐㅜ : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblCachedLabel.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        public static string GetCurrentTime(HttpContext context)
+        {
+            return DateTime.Now.ToLongTimeString();
+        }
+    }
+}
